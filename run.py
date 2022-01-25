5f761da4b9f571846521c848a12cee11e06bb655
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-25 22:40:07 +0300

    Same fix

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 import os
 
-os.system("chmod -R 0777 /model")
+os.system("rm -rf /model/result")
 
 with open('/model/run.sh', 'w') as f:
     f.write("python3 /model/codes/main.py --exp_dir /model/experiments_BD/EGVSR/001 --mode test --model /model/pretrained_models/EGVSR_iter420000.pth --opt test.yml --gpu_id 0\n")
     f.write("chmod -R 0777 /model/result\n")
 
-os.system("chmod 0777 /model/run.sh")
 os.system("/model/run.sh")
 
