7696cbb9a56e5daa91eb7eb1d060f13d127dc1f4
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-25 22:45:43 +0300

    Same error

--- run.py
+++ run.py
@@ -6,5 +6,6 @@ with open('/model/run.sh', 'w') as f:
     f.write("python3 /model/codes/main.py --exp_dir /model/experiments_BD/EGVSR/001 --mode test --model /model/pretrained_models/EGVSR_iter420000.pth --opt test.yml --gpu_id 0\n")
     f.write("chmod -R 0777 /model/result\n")
 
+os.system("chmod +x /model/run.sh")
 os.system("/model/run.sh")
 
